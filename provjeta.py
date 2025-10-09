fffbc8029c951d7c746ba5ce7fd3d48546f7b006
JosipFr <josip.franciskovic1@skole.hr>
2025-10-09 18:17:20 +0200

    Add files via upload
    uuuuuuuljavi zadatak

---
+++ provjeta.py
@@ -0,0 +1,9 @@
+a=int(input("koliko si visok???"))
+b=int(input("koliko si star"))
+if a>=160 and a<=190:
+    if b>16:
+        print ("možeš ići na ride")
+    else:
+        print("nesmiješ hahhhahahahaha")
+else:
+    print("nesmiješ hahahahaha")
